4973ef4f5fa1f8b71a530bce0c2a32e76a3bbdd9
pbsulliv <pbsulliv@ucsc.edu>
2020-04-13 17:24:06 -0700

    Final Game

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // add object to existing scene
         scene.add.existing(this);  //add to existing, displayList, updateList
         this.isFiring = false;     //track rocket's firing status
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -18,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button (NOT spacebar)
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= 108) {
