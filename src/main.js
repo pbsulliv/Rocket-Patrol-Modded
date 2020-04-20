f10606dc0cfb12c8545fbe8571d0f3407e4facf9
pbsulliv <pbsulliv@ucsc.edu>
2020-04-19 20:34:47 -0700

    Added list of Mods to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+// Pierce Sullivan
+//CMPM 120
+
+//Mods:
+//Implement the speed increase that happens after 30 seconds in the original game (10)
+//Create a new scrolling tile sprite for the background (10)
+//Display the time remaining (in seconds) on the screen (15)
+//Create a new animated sprite for the Spaceship enemies (15)
+//Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (25) 
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits (25)
+//FACADE Based on 25 point new enemy, new ship has new artwork, has lower width, and uses sin function to change movement speed (25)
 
 let config = {
     type: Phaser.Auto,
